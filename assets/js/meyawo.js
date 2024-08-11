b783e1fee86c257e0d535b40fe5be45b0728e981
Shaine Lomenario <shainelomenario@gmail.com>
2024-08-10 21:32:14 -0400

    Changed styling of the overall portfolio.

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -12,7 +12,6 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-// smooth scroll
 $(document).ready(function(){
     $(".navbar .nav-link").on('click', function(event) {
 
@@ -36,3 +35,23 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var fullName = document.getElementById('typewriter-swe');
+
+    var typewriter = new Typewriter(fullName, {
+        loop: true,   // Loop through the entire sequence
+        delay: 75     // Adjust typing speed if needed
+    });
+
+    typewriter
+        .typeString('Software Engineer')
+        .pauseFor(2500)
+        .deleteChars(21) 
+        .pauseFor(500)
+        .typeString('AI Engineer')
+        .pauseFor(2500)
+        .start();
+        
+});
+
